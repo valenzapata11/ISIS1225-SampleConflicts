c34baa4e9e82a000aaf58679911641907a6b5b63
valenzapata11 <v.zapatac@uniandes.edu.co>
2021-06-15 15:20:50 -0500

    Cambios Estudiante 1

--- App/model.py
+++ App/model.py
@@ -72,6 +72,8 @@ def createTagList():
     taglist = lt.newList(datastructure='SINGLE_LINKED')
     return taglist
 
-def addBookTags():
+def addBookTags(booktagsfile):
     # TO-DO: Modificación de Est-1 y Est-2 en el Lab 2
-    pass
+    booktags = lt.newList(datastructure='SINGLE_LINKED',
+    filename=booktagsfile)
+    return booktags
