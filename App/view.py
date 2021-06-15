d9d1f4905e10fc04fac23c7e307e2df14bce94e9
valenzapata11 <v.zapatac@uniandes.edu.co>
2021-06-15 15:25:50 -0500

    Cambios Main

--- App/view.py
+++ App/view.py
@@ -43,6 +43,7 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TO-DO: Modificación de Est-1 en el Lab 2
+    print("3- Cargar Book-Tags!")
     print("0- Salir")
 
 
